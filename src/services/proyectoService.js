5450f0338f56e6d419752bab11e10c9cf96181a7
MateoGB20 <homelobene@gmail.com>
2026-05-13 23:25:33 -0300

    services.js Mateo

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,5 +1,6 @@
 // src/services/proyectoService.js
-
+    // Arreglo privado de proyectos.
+    // Cada objeto representa un proyecto.
 const proyectoService = (() => {
     let proyectos = [
         { id: 1, titulo: "C++", categoria: "Programación", estado: "Finalizado" },
@@ -8,22 +9,27 @@ const proyectoService = (() => {
         { id: 4, titulo: "JavaScript", categoria: "Interactividad", estado: "En curso" },
         { id: 5, titulo: "Python", categoria: "Data Science", estado: "Pendiente" },
     ];
-
+    // Retorna una copia del arreglo de proyectos.
+    // Se utiliza spread operator para evitar modificar
+    // el arreglo original directamente.
     const obtenerProyectos = () => [...proyectos]; 
+        // Agrega un nuevo proyecto al arreglo.
     const agregarProyecto = (nuevo) => { 
         proyectos.push(nuevo);
     };
-
+    // filter genera un nuevo arreglo excluyendo
+        // el proyecto cuyo id coincida.
     const eliminarProyecto = (id) => { 
         proyectos = proyectos.filter(p => p.id !== id);
     };
-
+        // toLowerCase permite comparar sin importar mayúsculas.
+        // includes verifica si el texto está contenido en el título.
     const buscarProyecto = (texto) => {
         return proyectos.filter(p => 
             p.titulo.toLowerCase().includes(texto.toLowerCase())
         );
     };
-
+ // Se retornan únicamente las funciones públicas del módulo.
     return {
         obtenerProyectos,
         agregarProyecto,
@@ -32,5 +38,6 @@ const proyectoService = (() => {
     };
 })();
 
- 
+ // Exportación de las funciones para poder utilizarlas
+// desde otros archivos del proyecto.
 export const { obtenerProyectos, agregarProyecto, eliminarProyecto, buscarProyecto } = proyectoService;
