42ed17a865effd36509127dc600f5628f71dfa93
valenn01 <estradavalentin98@gmail.com>
2026-05-23 22:42:36 -0300

    proyectoService estrada valen

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -56,28 +56,44 @@ const proyectoService = (() => {
 
         },
     ];
-    // Retorna una copia del arreglo de proyectos.
-    // Se utiliza spread operator para evitar modificar
-    // el arreglo original directamente.
-    const obtenerProyectos = () => [...proyectos]; 
-        // Agrega un nuevo proyecto al arreglo.
+
+    
+    if (!localStorage.getItem("mis_proyectos_tp")) {
+        localStorage.setItem("mis_proyectos_tp", JSON.stringify(proyectos));
+    }
+
+    // Retorna una copia del arreglo de proyectos leyendo desde localStorage.
+    const obtenerProyectos = () => {
+        const guardados = localStorage.getItem("mis_proyectos_tp");
+        proyectos = JSON.parse(guardados); 
+        return [...proyectos]; 
+    }; 
+
+    // Agrega un nuevo proyecto al arreglo y actualiza el localStorage.
     const agregarProyecto = (nuevo) => { 
         const nuevoId = proyectos.length > 0 ? Math.max(...proyectos.map(p => p.id)) + 1 : 1;
         proyectos.push({ id: nuevoId, ...nuevo });
+
+        localStorage.setItem("mis_proyectos_tp", JSON.stringify(proyectos));
     };
-    // filter genera un nuevo arreglo excluyendo
-        // el proyecto cuyo id coincida.
+
+    // filter genera un nuevo arreglo excluyendo el proyecto cuyo id coincida.
     const eliminarProyecto = (id) => { 
         proyectos = proyectos.filter(p => p.id !== id);
+
+        localStorage.setItem("mis_proyectos_tp", JSON.stringify(proyectos));
     };
-        // toLowerCase permite comparar sin importar mayúsculas.
-        // includes verifica si el texto está contenido en el título.
+
+    // toLowerCase permite comparar sin importar mayúsculas.
+    // includes verifica si el texto está contenido en el título.
     const buscarProyecto = (texto) => {
-        return proyectos.filter(p => 
+        const actuales = obtenerProyectos();
+        return actuales.filter(p => 
             p.titulo.toLowerCase().includes(texto.toLowerCase())
         );
     };
- // Se retornan únicamente las funciones públicas del módulo.
+
+    // Se retornan únicamente las funciones públicas del módulo.
     return {
         obtenerProyectos,
         agregarProyecto,
@@ -86,6 +102,6 @@ const proyectoService = (() => {
     };
 })();
 
- // Exportación de las funciones para poder utilizarlas
+// Exportación de las funciones para poder utilizarlas
 // desde otros archivos del proyecto.
 export const { obtenerProyectos, agregarProyecto, eliminarProyecto, buscarProyecto } = proyectoService;
