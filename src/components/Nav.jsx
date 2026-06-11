ed23187dd1a1ce59ebabf1a78a8b79e1c34be53d
valenn01 <estradavalentin98@gmail.com>
2026-06-10 22:27:57 -0300

    punto 2 estrada valen

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import fotoPerfil from '../assets/adriel.png';
+
+// descomenten este
+// import { UsuarioContext } from '../context/UsuarioContext'; 
+
+import fotoValen from '../assets/adriel.png';
+import fotoDefault from '../assets/sinfoto.png';
 
 function Nav() {
+  //borren este de abajo
+  const usuario = { nombre: 'valen', rol: 'Alumno' }; 
+
+  // descomenten esto
+  // const { usuario } = useContext(UsuarioContext); 
+
+  const fotoPerfil = usuario?.nombre?.toLowerCase() === 'valen' ? fotoValen : fotoDefault;
+
   return (
     <nav className="nav-menu"> 
       <ul className="nav-lista">
         <li className="nav-items"><Link to="/dashboard" className="nav-link">INICIO</Link></li>
         <li className="nav-items"><Link to="/proyectos" className="nav-link">PROYECTOS</Link></li>
         <li className="nav-items"><Link to="/contacto" className="nav-link">CONTACTO</Link></li>
+        
         <li className="nav-items perfil">
-          <span className="user-nombre"><Link to="/perfil" className="nav-link">valen</Link></span>
+          <div className="user-info">
+
+            <Link to="/perfil" className="nav-link user-profile-button">
+              <span className="user-nombre">{usuario?.nombre || 'Invitado'}</span>
+              <span className="user-rol">{usuario?.rol || 'Sin Rol'}</span> 
+            </Link>
+          </div>
           <img src={fotoPerfil} alt="fotito" className="avatar" />
         </li>
       </ul>
